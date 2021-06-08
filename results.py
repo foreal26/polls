5e3a61e29c916a00b5f2873b9b4d37dd032a7cae
foreal26 <foreal@indigosea.xyz>
2021-06-07 20:12:38 -0400

    add count of votes

--- results.py
+++ results.py
@@ -12,4 +12,5 @@ for i in glob(f"{directory}/*.json"):
 frame = pd.DataFrame(results)
 for symbol, group in frame.groupby("title"):
     print(f"Story: {symbol}")
+    print(f"Total Votes: {group['title'].count()}")
     print(group["score"].value_counts())
